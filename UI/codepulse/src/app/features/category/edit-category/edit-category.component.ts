95350cddbc81cccd0a7a8f6eff7075f1121dd951
parthk219 <129992000+parthk219@users.noreply.github.com>
2023-10-21 23:10:54 +0530

    Commit 2

--- UI/codepulse/src/app/features/category/edit-category/edit-category.component.ts
+++ UI/codepulse/src/app/features/category/edit-category/edit-category.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { CategoryService } from '../services/category.service';
+import { Category } from '../models/category.model';
 
 @Component({
   selector: 'app-edit-category',
@@ -10,18 +12,30 @@ import { Subscription } from 'rxjs';
 export class EditCategoryComponent implements OnInit,OnDestroy {
 id:string | null=null;
 paramsSubscription?:Subscription;
+category?: Category;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+    private categoryService: CategoryService) { }
 
   ngOnInit(): void {
 this.paramsSubscription=this.route.paramMap.subscribe({
   next:(params)=>{
     this.id=params.get('id');
+    if(this.id){
+      this.categoryService.getCategoryById(this.id)
+      .subscribe({
+        next:(response)=>{
+          this.category=response;
+        }
+      });
+    }
   }
  }
 
 );
-  } 
+  } onFormSubmit():void{
+    console.log(this.category)
+  }
   ngOnDestroy(): void {
     this.paramsSubscription?.unsubscribe();
   }
--- UI/codepulse/src/app/features/category/services/category.service.ts
+++ UI/codepulse/src/app/features/category/services/category.service.ts
@@ -1,3 +1,38 @@
+// import { Injectable } from '@angular/core';
+// import { AddCategoryRequest } from '../models/add-category-request.model';
+// import { Observable } from 'rxjs';
+// import { HttpClient } from '@angular/common/http';
+// import { Category } from '../models/category.model';
+// import { environment } from 'src/environments/environment.prod';
+
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class CategoryService {
+
+//   constructor(private http:HttpClient) { }
+//   // addCategory(){}
+
+//   addCategory(model:AddCategoryRequest): Observable<void>{
+//     return this.http.post<void>('https://localhost:44346/api/categories',model);
+//   }
+//   getAllCategories(): Observable<Category[]>{
+//     return this.http.get<Category[]>('https://localhost:44346/api/categories');    // use environment here
+
+//   }
+//   getCategoryById(id:string):Observable<Category>{
+//     return this.http.get<Category>('https://localhost:44346/api/categories/${id}');    // /api/Categories/{id}
+
+//   }   
+// }
+// // https://localhost:44346/
+
+
+// // getCategoryById(id: string): Observable<Category> {
+// //   return this.http.get<Category>(`${environment.apiBaseUrl}/api/categories/${id}`);
+
+
 import { Injectable } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-request.model';
 import { Observable } from 'rxjs';
@@ -5,21 +40,22 @@ import { HttpClient } from '@angular/common/http';
 import { Category } from '../models/category.model';
 import { environment } from 'src/environments/environment.prod';
 
-
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  constructor(private http:HttpClient) { }
-  // addCategory(){}
+  constructor(private http: HttpClient) { }
 
-  addCategory(model:AddCategoryRequest): Observable<void>{
-    return this.http.post<void>('https://localhost:44346/api/categories',model);
+  addCategory(model: AddCategoryRequest): Observable<void> {
+    return this.http.post<void>('https://localhost:44346/api/categories', model);
   }
-  getAllCategories(): Observable<Category[]>{
-    return this.http.get<Category[]>('https://localhost:44346/api/categories');    // use environment here
 
+  getAllCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>('https://localhost:44346/api/categories'); // use environment here
+  }
+
+  getCategoryById(id: string): Observable<Category> {
+    return this.http.get<Category>(`https://localhost:44346/api/categories/${id}`); // use backticks for template string `````````Works now !!!!
   }
 }
-// https://localhost:44346/
